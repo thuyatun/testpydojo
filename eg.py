971f33cacbda7e5620a985b57b8fe5e9e1b7bc99
thuyatun <tunthura97@gmail.com>
2019-10-06 17:45:41 +0630

    Update eg.py
    write my name sentence and symbols

--- eg.py
+++ eg.py
@@ -1,5 +1,6 @@
-print ("hello" *3)
-print (3 **7)
-name="mgmg"
-age="20"
-print("My name is "+name+"."+" I am "+age+" years old.")
+print ("my name is Thu Ya Tun")
+print ("********  **    **     ******")
+print ("********   **  **      ******")
+print ("   **        **          **")
+print ("   **        **          **")
+print ("   **        **          **")
